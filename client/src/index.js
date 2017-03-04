a3d02d14c28cbf583d4803a1d44a85b8b5dc16dc
bmeritter <bme_ritter@foxmail.com>
2017-03-04 15:52:11 +0800

    test flash

--- client/src/index.js
+++ client/src/index.js
@@ -2,10 +2,18 @@ import React from 'react';
 import ReactDom from 'react-dom';
 
 function html() {
-  return {__html:'<span>hello world</span>'};
+  return {__html:'<h1>hello world!</h1>'};
+}
+
+function flash() {
+  return {__html:"<embed src='http://player.youku.com/player.php/sid/XMjUzNDIyNjM3Ng==/v.swf' allowFullScreen='true' quality='high' width='480' height='400' align='middle' allowScriptAccess='always' type='application/x-shockwave-flash'></embed>"};
 }
 
 ReactDom.render(
-  <div dangerouslySetInnerHTML={html()}></div>,
+  <div>
+    <div dangerouslySetInnerHTML={html()}></div>
+    <div dangerouslySetInnerHTML={flash()}></div>
+
+  </div>,
     document.getElementById('app')
 );
